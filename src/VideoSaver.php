82a309d0c87727d9c10c95c56c5339ab1d0b647c
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-05-21 09:44:45 -0400

    Add files via upload

---
+++ src/VideoSaver.php
@@ -0,0 +1,23 @@
+<?PHP
+    
+namespace YouTube;
+class VideoSaver extends YouTubeStreamer {
+  
+  protected $file_name;
+  
+  public function headerCallback($ch,$data){
+    parent::headerCallback($ch,$data);
+    $name = $this->file_name;
+    $this->sendHeader("Content-Disposition: attachment, filename=\"".$name."\"");
+  }
+  
+  public function setDownloadedFileName($name){
+    $this->file_name = $name;
+  }
+  
+  public function download($url){
+    parent::stream($url);
+  }
+}
+    
+?>
