c865473513e595636bb95dd68c74d4c4576abe66
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-30 13:41:42 -0400

    Add files via upload

---
+++ public/merge.php
@@ -0,0 +1,15 @@
+<?PHP
+    
+$video = isset($_REQUEST['video']) ? $_REQUEST['video'] : null;
+
+$audio = isset($_REQUEST['audio']) ? $_REQUEST['audio'] : null;
+
+$name = isset($_REQUEST['name']) ? $_REQUEST['name'].".mp4" : "output.mp4";
+
+if(!isset($video,$audio)){
+  die('Video or audio not found!');
+}
+
+$cmd = "ffmpeg -i $video -i $audio -c:v copy -c:a aac $name";
+
+echo shell_exec($cmd);
