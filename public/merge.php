74a45a66ed5c449b3027f7b1f16a4a26b431d967
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-30 15:46:56 -0400

    Add files via upload

--- public/merge.php
+++ public/merge.php
@@ -4,7 +4,11 @@ $streamer = "https://darrylmcoder-ytapp.herokuapp.com/stream.php?url=";
 
 $video = isset($_REQUEST['video']) ? $streamer.$_REQUEST['video'] : null;
 
+echo $video;
+
 $audio = isset($_REQUEST['audio']) ? $streamer.$_REQUEST['audio'] : null;
+
+echo $audio;
 
 $name = "output.mp4";
 
@@ -14,4 +18,4 @@ if(!isset($video,$audio)){
 
 $cmd = "ffmpeg -i '$video' -i '$audio' -c:v copy -c:a aac '$name'";
 
-echo shell_exec($cmd);
+shell_exec($cmd);
