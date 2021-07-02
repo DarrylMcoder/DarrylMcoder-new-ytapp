6237c254ae2248eefed249d7dc8662fdec9da37b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-07-02 16:56:29 -0400

    Add files via upload

--- public/database_config.php
+++ public/database_config.php
@@ -7,7 +7,7 @@ $parts = parse_url($url);
 define('DB_SERVER', $parts['host']);
 define('DB_USERNAME', $parts['user']);
 define('DB_PASSWORD', $parts['pass']);
-define('DB_NAME', 'YTAPP');
+define('DB_NAME', 'mqpklhdps7eegv9f');
  
 /* Attempt to connect to MySQL database */
 $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
