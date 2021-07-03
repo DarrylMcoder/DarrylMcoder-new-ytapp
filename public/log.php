0d8d10404fec11202bf2c17aa0815e78f6864d7b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-07-03 09:41:06 -0400

    Add files via upload

--- public/log.php
+++ public/log.php
@@ -1,6 +1,6 @@
 <?PHP
     
-require('database_config.php');
+include('database_config.php');
 
 $download_date = isset($_POST['download_date']) ? $_POST['download_date'] : null;
 
@@ -10,17 +10,17 @@ $url = isset($_POST['url']) ? $_POST['url'] : null;
 
 $sql = "CREATE TABLE IF NOT EXISTS Videos(
   ID INT AUTO_INCREMENT,
-  download_date VARCHAR,
+  download_date VARCHAR(255),
   name VARCHAR(255) UNIQUE,
   downloads INT,
-  url VARCHAR(1000),
+  url VARCHAR(1000) UNIQUE,
   PRIMARY KEY(ID)
 )";
 
 $mysqli->query($sql);
 
 $sql = "INSERT INTO Videos(
-download_date,name,url) 
-VALUES('$download_date','$name','$url')";
+download_date,name,downloads,url) 
+VALUES('$download_date','$name',1,'$url') ON DUPLICATE KEY UPDATE downloads = downloads + 1,download_date = '$download_date'";
 
 $mysqli->query($sql);
