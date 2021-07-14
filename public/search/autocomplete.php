1ba6d5d9fe40a98b201fa4a4b001ad5eea4a4949
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-07-14 13:46:50 -0400

    Add files via upload

--- public/search/autocomplete.php
+++ public/search/autocomplete.php
@@ -5,5 +5,5 @@ require('../../vendor/autoload.php');
 $term = isset($_GET['term']) ? $_GET['term'] : null;
 
 $yt = new \YouTube\YouTubeDownloader();
-$json = $yt-getSearchSuggestions($term);
+$json = $yt->getSearchSuggestions($term);
 echo json_encode($json);
