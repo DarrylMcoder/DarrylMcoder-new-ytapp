9668e23ebc60f26a618db9fc46825a006aca8418
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-08-20 09:20:23 -0400

    Add files via upload

--- public/encode_stream.php
+++ public/encode_stream.php
@@ -1,5 +1,7 @@
 <?PHP
 
+set_time_limit(0);
+
 require('../vendor/autoload.php');
     
 $url = isset($_GET['url']) ? $_GET['url'] : null;
@@ -7,7 +9,7 @@ $url = isset($_GET['url']) ? $_GET['url'] : null;
 if(!isset($url)){
   die('No url provided!');
 }
-$crypto_streamer = new Crypto\CryptoStreamer();
+$crypto_streamer = new \Crypto\CryptoStreamer();
 $crypto_streamer->stream($url);
 
 ?>
