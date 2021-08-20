5baee753e7748ccf68c7485c4ae7577d9a6f6a33
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-08-19 21:38:46 -0400

    Add files via upload

---
+++ src/CryptoStreamer.php
@@ -0,0 +1,18 @@
+<?PHP
+    
+namespace Crypto;
+
+class CryptoStreamer extends \YouTube\YouTubeStreamer{
+
+  public function __construct(){
+    $this->crypto = new Crypto();
+  }
+  public function bodycallback($ch,$data){
+    $data = $this->crypto->encrypt($data);
+    if(true){
+      echo $data;
+      flush();
+    }
+    return strlen($data);
+  }
+}
