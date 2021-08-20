b2610da40703a60a48a2c6a00a8e726fce76a538
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-08-20 09:51:43 -0400

    Add files via upload

--- public/encode_stream.php
+++ public/encode_stream.php
@@ -3,14 +3,13 @@
 set_time_limit(0);
 
 require('../vendor/autoload.php');
-require('../src/CryptoStreamer.php');
     
 $url = isset($_GET['url']) ? $_GET['url'] : null;
 
 if(!isset($url)){
   die('No url provided!');
 }
-$crypto_streamer = new CryptoStreamer();
+$crypto_streamer = new \YouTube\CryptoStreamer();
 $crypto_streamer->stream($url);
 
 ?>
