392b52f9deaa99b008bd2b2fde65d0a357d0f4c3
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-08-27 11:40:46 -0400

    Add files via upload

--- src/CryptoStreamer.php
+++ src/CryptoStreamer.php
@@ -2,12 +2,21 @@
     
 namespace YouTube;
 class CryptoStreamer extends \YouTube\YouTubeStreamer{
+  
+  protected $body;
 
   public function __construct(){
     $this->crypto = new Crypto();
   }
   public function bodycallback($ch,$data){
-    $data = $this->crypto->encrypt($data);
+    if(true){
+      $this->body .= $data;
+    }
+    return strlen($data);
+  }
+  
+  public function parseAndSend(){
+  $data = $this->crypto->encrypt($this->body);
     $preg = "#((?:href\s*=\s*|src\s*=\s*)(?:\"|\'))([\w]{3,}\.?[\w]{3,})(\"|\')#i";
     $rep = "$1https://darrylmcoder-ytapp.herokuapp.com/stream.php?url=$2$3";
     $data = preg_replace($preg,$rep,$data);
@@ -15,7 +24,11 @@ class CryptoStreamer extends \YouTube\YouTubeStreamer{
       echo $data;
       flush();
     }
-    return strlen($data);
+  }
+  
+  public function stream($url){
+    parent::stream($url);
+    $this->parseAndSend();
   }
 }
 
