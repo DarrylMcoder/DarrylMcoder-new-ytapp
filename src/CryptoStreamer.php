b3e4d4060529f288f57f6287bd05b7a1a9b0aff1
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-07 11:02:01 -0400

    Add files via upload

--- src/CryptoStreamer.php
+++ src/CryptoStreamer.php
@@ -16,8 +16,8 @@ class CryptoStreamer extends \YouTube\YouTubeStreamer{
   }
   
   public function parseAndSend(){
+    echo $this->crypto->encrypt("\n\nPARSEandSEND\n\n");
     $data = $this->body;
-    
     $data = preg_replace_callback( "#((?:src|(?<!a )href|action|data)\s?=\s?)(\"|')(.*?)\2#i", [$this,'proxify'], $data);
     $data = $this->crypto->encrypt($data);
     if(true){
