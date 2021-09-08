ed024bdc6378b6149c35ac30ad831e80ac92812e
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-08 09:00:47 -0400

    Add files via upload

---
+++ public/streamdump.php
@@ -0,0 +1,19 @@
+<?PHP
+    
+set_time_limit(0);
+
+require('../vendor/autoload.php');
+
+$url = isset($_GET['url']) ? $_GET['url'] : null;
+
+if(isset($_GET['crypt']) && $_GET['crypt'] == 'on'){
+$url = base64_decode($url);
+}
+
+if ($url == false) {
+    die("No url provided");
+}
+
+$youtube = new \YouTube\YouTubeDownloader();
+$links = $youtube->getDownloadLinks($url);
+var_dump($links);
