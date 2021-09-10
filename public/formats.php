169ccfb4d6d1f054318a1c59a79fdd73a0ec44c3
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-10 14:51:42 -0400

    Add files via upload

--- public/formats.php
+++ public/formats.php
@@ -40,6 +40,13 @@ $formats = $links->getAllFormats();
         padding: 0;
         font: Arial 14px;
       }
+      .mainitem{
+        background-color:purple;
+        color:white;
+        border-radius:5px;
+        padding:3px;
+        text-decoration:none;
+      }
     </style>
     <link rel="stylesheet" href="http://static.darrylmcoder.epizy.com/assets/style.css"/>
     <script defer src="http://static.darrylmcoder.epizy.com/assets/script.js"></script>
@@ -61,17 +68,17 @@ $formats = $links->getAllFormats();
 echo "<h3>".$name."</h3><br>";
     ?>
       <a href="download.php?n=<?=$name?>&url=<?=urlencode($combined->url)?>">
-        <div class="listitem">
+        <div class="listitem mainitem">
             Download <?=$combined->qualityLabel?> video with audio: <?php echo round($combined->contentLength / 1000000,1)."mb"; ?>
         </div>
       </a>
       <a href="download.php?n=<?=$name?>&url=<?=urlencode($best->audio->url)?>">
-        <div class="listitem">
+        <div class="listitem mainitem">
             Download highest quality audio: <?php echo round($best->audio->contentLength / 1000000,1)."mb"; ?>
         </div>
       </a>
       <a href="download.php?n=<?=$name?>&url=<?=urlencode($best->video->url)?>">
-        <div class="listitem">
+        <div class="listitem mainitem">
             Download <?=$best->video->qualityLabel?> video: <?php echo round($formats[1]->contentLength / 1000000,1)."mb"; ?>
         </div>
       </a>
