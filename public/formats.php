5da54938d027e31886620fb2dd9a72ab99fec548
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-11 15:11:53 -0400

    Add files via upload

--- public/formats.php
+++ public/formats.php
@@ -46,8 +46,7 @@ $url = base64_decode($url);
       </div>
 
 <?php
-      
-if(isset($url) :
+     
 
 $youtube = new \YouTube\YouTubeDownloader();
 try{
@@ -114,118 +113,7 @@ echo "<h3>".$name."</h3><br>";
 ?>
         </div>
       </div>
-      <?php elseif(!isset($url)) : ?>
 
-      
-    <a href="https://t.me/joinchat/zGTCgHpvKN9iMWYx">
-      <img src="stream.php?url=http://ytapp.darrylmcoder.epizy.com/img/telegram.png" width="50" height="50"/><img src="stream.php?url=http://ytapp.darrylmcoder.epizy.com/img/cloudveil.png" width="50" height="50"><br>
-    Join our Telegram group!
-    </a><br><br>
-    <form action="" method="get">
-  <input type="text" value= "<?=$_GET['url']; ?>" class="input" id="txt_url" name="url"><br>
-  <button type="button" class="go" id="btn_fetch" value="Fetch">
-  Fetch
-  </button>
-  <button type="submit" class="go">
-    Save
-  </button>
-</form>
-    <h3 id="name">
-      
-    </h3>
-
-  <video width="100%" controls>
-    <source src="" type="video/mp4"/>
-    <em>Sorry, your browser doesn't support HTML5 video.</em>
-</video>
-    
-    <p id="description">
-      
-    </p>
-
-
-<script>
-    $(function () {
-
-        $("#btn_fetch").click(function () {
-
-            var url = $("#txt_url").val();
-
-            var oThis = $(this);
-            oThis.attr('disabled', true);
-
-            $.get('video_info.php', {url: url}, function (data) {
-
-                console.log(data);
-
-                oThis.attr('disabled', false);
-
-                var links = data['links'];
-                var error = data['error'];
-                var name = data['name'];
-                var description = data['description'];
-              
-                $("#name").html(name);
-                $("#description").html(description);
-
-                if (error) {
-                    alert('Error: ' + error);
-                    return;
-                }
-
-                // first link with video
-                var first = links[0];
-
-                if (typeof first === 'undefined') {
-                    alert('No video found!');
-                    return;
-                }
-
-                var stream_url = 'stream.php?url=' + encodeURIComponent(first);
-
-                var video = $("video");
-                video.attr('src', stream_url);
-                video[0].load();
-               var today = new Date();
-          var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-          var time = today.getHours() + ":" + today.getMinutes();
-          var dateTime = date+' '+time;
-          
-          
-              $.post("log.php",{
-                download_date: dateTime,
-                name: name,
-                url: url
-              },function(data) {
-                //process result here
-              });
-            });
-
-        });
-
-    });
-  
-  function isPlaylist(url){
-    if(url.includes('/playlist')){
-      return true;
-    }else{
-      return false;
-    }
-  }
-  
-    
-  function isVideo(url){
-    if(url.includes('/watch')){
-      return true;
-    }else{
-      return false;
-    }
-  }
-</script>
-      
-      
-      <?php endif; ?>
-      
     </div>
     <?php echo file_get_contents("http://static.darrylmcoder.epizy.com/assets/footer.html"); ?>
   </body>
