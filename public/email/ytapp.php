c86d44a2970ac35267193e41b29e5fba422af5a8
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-22 11:15:49 -0400

    Add files via upload

--- public/email/ytapp.php
+++ public/email/ytapp.php
@@ -1,5 +1,15 @@
 <?PHP
     
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../src/PHPMailer/src/Exception.php';
+
+require '../../src/PHPMailer/src/PHPMailer.php';
+
+require '../../src/PHPMailer/src/SMTP.php';
+
+
 require('../../vendor/autoload.php');
 $imap_driver = new \YouTube\imap_driver();
 $imap_driver->full_debug = true;
@@ -33,12 +43,47 @@ foreach($ids as $uid){
   }
   var_dump($headers);
   $from = $headers['from'];
+  preg_match("#(.*?)<(.*?)>#i",$from,$m);
+  $from_name = $m[1];
+  $from_addr = $m[2];
   $subj = $headers['subject'];
   $url = $subj;
   $browser = new \YouTube\Browser();
   $page = $browser->get($url);
-  if(mail($from,"Email Browser",$page->body) === false){
+  
+  
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'darrylmcoder.ytapp@gmail.com';                     //SMTP username
+    $mail->Password   = 'ytapp@ytapp';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('darrylmcoder.ytapp@gmail.com', 'Email Browser');
+    $mail->addAddress($from_addr, $from_name;     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Email Browser';
+    $mail->Body    = $page->body;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+  
+  /*if(mail($from,"Email Browser",$page->body) === false){
     echo "mail() failed:";
     exit;
-  }
+  }*/
 }
