035e21c9690544bd170ef2cb9002d01889a85d5b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-11-11 19:39:01 -0500

    Add files via upload

--- src/functions.php
+++ src/functions.php
@@ -34,6 +34,24 @@ function crypt_enable(){
     echo "<!DOCTYPE html>
 <html>
   <head>
+      <meta name=\"viewport\" content=\"width=320, initial-scale=1\">
+    <meta charset=\"utf-8\">
+    <style>
+      body, html {
+        min-width: 100%;
+        min-height: 100%;
+        margin: 0;
+        padding: 0;
+        font: Arial 14px;
+      }
+      
+      #mainitem{
+        background-color: white;
+        border-radius:50px;
+        color:black;
+        vertical-align:center;
+      }
+    </style>
    <link rel=\"stylesheet\" href=\"http://static.darrylmcoder.epizy.com/assets/style.css\"/>
     <script defer src=\"http://static.darrylmcoder.epizy.com/assets/script.js\"></script>
   </head>
